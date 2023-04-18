e98c15b2c1aeaf5a46efb9491fbab8204a70350e
Olivia Brotherus <olivia@brotherus.fi>
2023-04-18 11:48:02 +0300

    -autotpep ja taskit

--- tasks.py
+++ tasks.py
@@ -14,5 +14,12 @@ def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
 
+@task
+def lint(ctx):
+   ctx.run("pylint src", pty=True)
 
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
 
